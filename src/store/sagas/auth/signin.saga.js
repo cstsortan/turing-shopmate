04aa058b5198973c611739aba3e7911bb1afa31b
Tsortanidis Christos <tsortanidis.ch@gmail.com>
2019-07-19 00:03:08 +0300

    fixes typo on customer response

--- src/store/sagas/auth/signin.saga.js
+++ src/store/sagas/auth/signin.saga.js
@@ -2,13 +2,15 @@ import {put, takeLatest, call} from 'redux-saga/effects';
 import authService from "../../../services/authService";
 import { SIGNIN_REQUESTED, signinSuccess, SIGNIN_FAILED } from '../../actions/auth/signin.action';
 import { changeAuthState } from '../../actions/auth';
+import { hideAuth } from '../../actions/alerts';
 
 function* signInSaga(action) {
     try {
         const { email, password } = action.payload;
         const data = yield call(authService.signIn, email, password);
         yield put(signinSuccess(data));
-        yield put(changeAuthState(data.user));
+        yield put(changeAuthState(data.customer));
+        yield put(hideAuth());
     } catch (error) {
         yield put({
             type: SIGNIN_FAILED, payload: error
